04f17f340d8a0765fd457d5e98db8f5e44137196
vagrant <vagrant@homestead>
2019-12-11 06:26:36 +0000

    登录时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -15,12 +15,18 @@ class SessionsController extends Controller
             'email' => 'required|email|max:255',
             'password' => 'required'
         ]);
-        if(Auth::attempt($credentials)){
+        if(Auth::attempt($credentials, $request->has('remmber'))){
             session()->flash('success', '欢迎回来');
             return redirect()->route('users.show', [Auth::user()]);
         }else{
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
         }
+    }
+
+    public function destory(){
+        Auth::logout();
+        session()->flash('success', '您已经退出');
+        return redirect('login');
     }
 }
