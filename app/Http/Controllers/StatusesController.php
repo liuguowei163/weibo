122dc29fbce94b5ac6633fb46becc1af07fbbcd8
vagrant <vagrant@homestead>
2019-12-12 08:49:28 +0000

    删除微博

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use  Auth;
+use App\Models\Status;
 
 class StatusesController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+//    public function __construct()
+//    {
+////        $this->middleware('auth');
+//    }
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -21,6 +22,13 @@ class StatusesController extends Controller
             'content' => $request['content']
         ]);
         session()->flash('success', '发布成功！');
+        return redirect()->back();
+    }
+    public function destroy(Status $status)
+    {
+        $this->authorize('destroy', $status);
+        $status->delete();
+        session()->flash('success', '微博已被成功删除！');
         return redirect()->back();
     }
 }
